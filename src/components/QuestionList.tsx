1f193e7aa85a368cf40066166de437b94c94a02c
chaeyoonjeong <lily9991@kaist.ac.kr>
2021-05-25 01:24:46 +0900

    Add question list drawer

--- src/components/QuestionList.tsx
+++ src/components/QuestionList.tsx
@@ -4,31 +4,40 @@ import styled from 'styled-components';
 import { Drawer, Box, List, ListItem, ListItemText, Typography, Divider } from '@material-ui/core';
 import { PINK_3 } from 'utils/themes';
 
-export const QuestionList = (props: QuestionListHeaderProp) => {
-  //const QuestionListElement :
+interface QuestionListHeaderProp {
+  topic: string;
+  subtopic: string;
+}
+interface QuestionListElementProp {
+  title: string;
+  content: string;
+  timestamp: string;
+}
+
+export const QuestionList: FC = () => {
   const QuestionListHeaderContent: QuestionListHeaderProp = {
     topic: 'Project 1',
     subtopic: 'Alarm-Clock',
   };
-  const QuestionListElementContent: QuestionListElement ={
-      title: "Regarding 'disk.c'",
-      content: "I had a question regarding disk.c. When I added line thread_yield in sema_up, disk.c failed ...",
-      timestamp: '3 hours ago'
-  }
+  const QuestionListElementContent: QuestionListElementProp = {
+    title: "Regarding 'disk.c'",
+    content: 'I had a question regarding disk.c. When I added line thread_yield in sema_up, disk.c failed ...',
+    timestamp: '3 hours ago',
+  };
 
   return (
-    <Drawer variant="permanent" anchor="left">
+    <QuestionListDrawer variant="permanent" anchor="left">
       <QuestionListHeader>
-          {QuestionListHeaderContent.topic} {'>'} {QuestionListHeaderContent.subtopic}
+        {QuestionListHeaderContent.topic} {'>'} {QuestionListHeaderContent.subtopic}
       </QuestionListHeader>
-      <QuestionListDrawer>
+      <QuestionListDrawerBody>
         <QuestionListElement button>{QuestionListElementContent.title}</QuestionListElement>
-      </QuestionListDrawer>
-    </Drawer>
+        <Divider />
+      </QuestionListDrawerBody>
+    </QuestionListDrawer>
   );
 };
 
-const QuestionListDrawerWidth = 378;
 const QuestionListElement = styled(ListItem)`
   :hover {
     background-color: #cdcdcd;
@@ -37,20 +46,16 @@ const QuestionListElement = styled(ListItem)`
     background-color: #{PINK_3};
   }
 `;
-
-const QuestionListDrawer = styled(List)``;
+const QuestionListDrawer = styled(Drawer)`
+  .MuiDrawer-paperAnchorLeft {
+    left: 15%;
+    right: auto;
+    top: 4vh;
+  }
+  width: 25%;
+`;
+const QuestionListDrawerBody = styled(List)``;
 
 const QuestionListHeader = styled(Box)`
   font-family: Lato;
 `;
-
-type QuestionListElement = {
-  title: string;
-  content: string;
-  timestamp: string;
-};
-
-type QuestionListHeaderProp = {
-  topic: string;
-  subtopic: string;
-};
