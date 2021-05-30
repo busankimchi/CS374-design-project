35387a9ef800537c08fa6cde7ed26d02a43fbe71
busankimchi <jacky185@kaist.ac.kr>
2021-05-30 18:28:31 +0900

    Fix lint

--- src/components/Contents/index.ts
+++ src/components/Contents/index.ts
@@ -5,6 +5,7 @@ import { FAQButton } from './FAQButton';
 import { Hover } from './Hover';
 import { NoAnswer } from './NoAnswer';
 import { NotSelected } from './NotSelected';
+import { ShadowBox } from './ShadowBox';
 import { UserInfo } from './UserInfo';
 
-export { AnswerDisplay, AnswerDivider, Contents, FAQButton, Hover, NoAnswer, NotSelected, UserInfo };
+export { AnswerDisplay, AnswerDivider, Contents, FAQButton, Hover, NoAnswer, NotSelected, ShadowBox, UserInfo };
--- src/pages/SpecialQuestion.tsx
+++ src/pages/SpecialQuestion.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from 'react';
+import { FC } from 'react';
 import styled from 'styled-components';
 import { Backdrop, Box } from '@material-ui/core';
 import { PageType } from 'utils/types';
